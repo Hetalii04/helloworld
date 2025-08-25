d3af9a3647711fb11735a5ed95b5cd4d0e088d25
Hetali <hetalihp4595@gmail.com>
2025-08-25 11:43:20 +0530

    C file

--- fibonacci.c
+++ fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n, a = 0, b = 1, next;
+    int n, a = 0, b = 1, c;
     
     printf("Enter number of terms: ");
     scanf("%d", &n);
@@ -9,10 +9,10 @@ int main() {
     printf("Fibonacci sequence: %d %d ", a, b);
     
     for (int i = 2; i < n; i++) {
-        next = a + b;
+        c = a + b;
         printf("%d ", next);
         a = b;
-        b = next;
+        b = c;
     }
     
     printf("\n");
